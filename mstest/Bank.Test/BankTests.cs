b49e7c82999588b2bb0ad2402ace3d272843f91a
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-03-10 12:19:23 +0300

    add mstest

---
+++ mstest/Bank.Test/BankTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Bank.Modules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank.Test
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            // arrange
+            int amount = 100;
+            int payment = 100;
+            int expected = 0;
+            var bankAccount = new BankAccount(amount);
+
+            // act
+            bankAccount.Withdraw(payment);
+
+            // assert
+            Assert.AreEqual(expected, bankAccount.Balance, "AAA.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test2()
+        {
+            // arrange
+            int amount = 100;
+            int payment = 200;
+            var bankAccount = new BankAccount(amount);
+
+            // act
+            bankAccount.Withdraw(payment);
+
+            // assert
+        }
+    }
+}
