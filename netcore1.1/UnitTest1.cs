695bfdec32a08918f25b1a836b969f6a1ee8253c
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-03-11 19:28:16 +0300

    Add netcore1.1 project

---
+++ netcore1.1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TeestMethod1()
+        {
+        }
+    }
+}
